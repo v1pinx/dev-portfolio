989c2657556b9fe6a4094240e2f2d907a8931f33
Vipin Kumar <vkprajapati665@gmail.com>
2024-10-17 03:26:31 +0530

    added: added prop func

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+'use client'
 import CustomCursor from "./components/CustomCursor/CustomCursor";
 import Footer from "./components/Footer/Footer";
 import Introduction from "./components/Introduction/Introduction";
@@ -6,18 +7,25 @@ import Project from "./components/Project/Project";
 import Skills from "./components/Skills/Skills";
 import SocialIcons from "./components/SocialIcons/SocialIcons";
 import { Analytics } from "@vercel/analytics/react"
+import { useState } from "react";
 
 
 export default function Home() {
+  const [activeComponent, setActiveComponent] = useState('introduction');
+
+  const handleMenuClick = (component : any) => {
+      setActiveComponent(component);
+  };
+
   return (
-    <>
-      <CustomCursor />
-      <Navbar />
-      {/* <SocialIcons /> */}
-      <Introduction />
-      {/* <Project /> */}
-      {/* <Skills /> */}
-      {/* <Footer /> */}
-    </>
+      <>
+          <CustomCursor />
+          <Navbar onMenuClick={handleMenuClick} />
+          {/* <SocialIcons /> */}
+
+          {activeComponent === 'introduction' && <Introduction />}
+          {activeComponent === 'project' && <Project />}
+          {activeComponent === 'skills' && <Skills />}
+      </>
   );
 }
