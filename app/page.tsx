e7111d50496e1bce3cf06a6fb6891e29758460c2
v1pinx <vkprajapati665@gmail.com>
2025-02-21 05:52:50 +0530

    minor fix

--- app/page.tsx
+++ app/page.tsx
@@ -8,12 +8,12 @@ import Resume from "./components/Resume/Resume";
 import Skills from "./components/Skills/Skills";
 import SocialIcons from "./components/SocialIcons/SocialIcons";
 import { Analytics } from "@vercel/analytics/react"
-import ProgressBar from "react-scroll-progress-bar";
+// import ProgressBar from "react-scroll-progress-bar";
 export default function Home() {
 
   return (
     <>
-      <ProgressBar height="3" bgcolor="#ffa400" duration="1" />
+      {/* <ProgressBar height="3" bgcolor="#ffa400" duration="1" /> */}
       {/* <Navbar /> */}
       <Introduction />
       <Skills />
